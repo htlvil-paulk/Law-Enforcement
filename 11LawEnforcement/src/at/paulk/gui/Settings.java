73cf30c4dd30660911fcc603baf0f033077286a0
schueler <schueler@ubuntu>
2018-06-04 18:27:52 +0200

    test

--- 11LawEnforcement/src/at/paulk/gui/Settings.java
+++ 11LawEnforcement/src/at/paulk/gui/Settings.java
@@ -4,7 +4,7 @@ import java.awt.Color;
 
 final class Settings
 {
-	public static final String APPLICATION_NAME = "Police Information Service";
+	public static final String APPLICATION_NAME = "Police Information Services";
 	public static final String POLICE_DEPARTMENT_NAME = "HTL Villach - Hall Supervision Riot Units";
 	public static final Color PRIMARY_COLOR = Color.LIGHT_GRAY;
 	public static final Color SECONDARY_COLOR = Color.CYAN;
